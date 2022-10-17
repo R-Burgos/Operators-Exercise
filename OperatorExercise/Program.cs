7d12d83408ab6bbb55b410e14d5bff8911648368
Ruben Burgos <code.rburgos@gmail.com>
2022-10-17 14:24:57 -0500

    Finished Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}");
+           
+            /*
+             * Thought Exercise
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+            Console.WriteLine(k); //16
+            Console.WriteLine(j); //5 ????
+            Console.WriteLine(i); //4
+            */
         }
     }
 }
